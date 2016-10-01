04784502e0c79bbd8a64a5229c1b8daa053a2574
Diego Bastos <thepastosonline@gmail.com>
2016-10-01 08:30:22 -0400

    Archivo modificado

--- HelloMoto3.java
+++ HelloMoto3.java
@@ -1,6 +1,7 @@
 public class HelloMoto {
 	public static void main (String []args){
 	Provider.getSomething(); 
-	System.out.println("Hello Moto"); 	
+	System.out.println("Hello Moto");
+	System out.println("Goodbye Moto"); 	
 	}
 }
